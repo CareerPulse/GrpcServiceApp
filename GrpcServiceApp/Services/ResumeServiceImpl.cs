e5f8bf89372887a0b38da0cc6d357fe6fd22dc92
Максим Антонов <antonov_md@omk.ru>
2025-02-03 16:50:15 +0300

    Правки

--- GrpcServiceApp/Services/ResumeServiceImpl.cs
+++ GrpcServiceApp/Services/ResumeServiceImpl.cs
@@ -1,23 +1,29 @@
 ﻿using Grpc.Core;
-using GrpcServiceApp;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Firefox;
 
 namespace GrpcServiceApp.Services
 {
-    public class ResumeServiceImpl : GrpcServiceApp.ResumeService.ResumeServiceBase
+    public class ResumeServiceImpl : ResumeService.ResumeServiceBase
     {
         public override async Task getResumeList(IAsyncStreamReader<ResumeRequest> requestStream, IServerStreamWriter<ResumeListResponse> responseStream, ServerCallContext context)
         {
             string name = requestStream.Current.Title;
             string sort = requestStream.Current.Sorting;
-
             ResumeListResponse resumes;
             HHResumeService service = new HHResumeService();
-
-            resumes = service.GetResumes(sort);
-
-            await responseStream.WriteAsync(resumes);
+            try
+            {
+                resumes = service.GetResumes(sort);
+                if(sort == "ASC")
+                    resumes.List.Order();
+                else if(sort == "DESC")
+                    resumes.List.OrderDescending(); 
+                await responseStream.WriteAsync(resumes);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                await responseStream.WriteAsync(new ResumeListResponse());
+            }
             
         }
     }
